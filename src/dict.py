d0f31e0fc4626ab3b1448671048964c4b01388e4
Sergey80 <ses.box@gmail.com>
2015-05-31 12:03:09 -0400

    dict +

--- src/dict.py
+++ src/dict.py
@@ -1,15 +1,48 @@
-# dict (no order, keys are immutable, values may be mutable)
+# dict (no order, keys _must be_ immutable and unique, values may be mutable)
+# #dict #update #copy #in #values #items #del
 
 print("----dicts")
 
-d = {'alice': "222", 'bob': "333"}
+d = {'alice': "222", 'bob': "333"}   # {}
 
 print(d['bob'])  # 333
+
+# iterating over the dictionary
 
 # for
 for k in d:
     print(k)  # alice, bob
 
 
-for v in d.values():
+for v in d.values():    # by value
     print(v)
+
+for key in d:   # by key
+    print("{key} => {value}".format(key=key, value=d[key]))  # keys and values
+
+for key, value in d.items():  # by items
+    print("{key} => {value}".format(key=key, value=value))
+
+names_and_ages = [('alice', 25), ('Bob', 28), ('James', 32)]
+d = dict(names_and_ages)  # converts array of tuples into dictionary
+
+print(d["Bob"])  # 28
+
+dCopy = d.copy()  # copy
+dCopy2 = dict(d)  # by dict() constructor
+
+print(dCopy)
+
+d2 = dict(one=1, two=2, three=3)
+print("d2:" + str(d2))
+
+d2.update(d)  # extend one dictionary by another
+print("d2 updated:" + str(d2))
+
+yesNo = "one" in d2
+print(yesNo)            # True
+
+del d2["Bob"]           # Bob item is removed from the dictionary
+print(d2)
+
+
